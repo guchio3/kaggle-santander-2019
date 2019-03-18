37058b4f26871f298d49171751ca112daeda26d7
naoya.taguchi <astt.hwhw@gmaiil.com>
2019-03-18 17:09:52 +0000

    made .gitignore

---
+++ setup_dir.sh
@@ -0,0 +1,21 @@
+# mkdirs
+mkdir notebooks
+mkdir tools
+mkdir tools/utils
+mkdir tools/models
+mkdir tools/features
+mkdir mnt
+# mkdir mnt/figs
+# mkdir mnt/importances
+# mkdir mnt/logs
+# mkdir mnt/oofs
+# mkdir mnt/submissions
+# mkdir mnt/trained_models
+# mkdir mnt/inputs
+# mkdir mnt/inputs/origin
+# mkdir mnt/inputs/
+
+# set .gitignore
+touch .gitignore
+echo "tags*" >> .gitignore
+echo "mnt/" >> .gitignore
