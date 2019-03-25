0191084a0cc0ebae09ab3b2c26351cc74557091b
naoya-taguchi <naoya.taguchi@dena.com>
2019-03-25 14:16:15 +0900

    updated under and over sampling func

--- tools/utils/samplings.py
+++ tools/utils/samplings.py
@@ -4,42 +4,46 @@ import pandas as pd
 from ..utils.logs import sel_log
 
 
-def get_neg_us_index(target, random_state=None):
+def get_binary_us_index(target, random_state=None):
     '''
-    Get negative undersamling index.
-    Positive and negative is not important here, so it can be more generalized.
+    Get binary undersamling index.
+
     '''
-    res_index = target[target == 1].index
-    res_index = res_index.append(target[target == 0].sample(
+    pos_size = target[target == 1].count()
+    neg_size = target[target == 0].count()
+    less_target = 1 if pos_size < neg_size else 0
+    more_target = (less_target + 1) % 2
+
+    res_index = target[target == less_target].index
+    res_index = res_index.append(target[target == more_target].sample(
         len(res_index), random_state=random_state).index)
     return res_index
 
 
-def get_pos_os_index(target, os_lim, random_state=None):
+def get_binary_os_index(target, os_lim, random_state=None):
     '''
-    Get positive oversamling index.
-    Positive and negative is not important here, so it can be more generalized.
+    Get binary oversamling index.
+
     '''
     pos_size = target[target == 1].count()
     neg_size = target[target == 0].count()
-    assert neg_size >= pos_size, \
-        f'The sample of pos is more than neg ! ({pos_size} : {neg_size})'
+    less_target = 1 if pos_size < neg_size else 0
 
-    os_num = min(neg_size - pos_size, os_lim)
+    os_num = min(abs(neg_size - pos_size), os_lim)
     res_index = target.index
-    res_index = res_index.append(target[target == 1].sample(
+    res_index = res_index.append(target[target == less_target].sample(
         os_num, replace=True, random_state=random_state).index)
     return res_index
 
 
 def resampling(target, resampling_type, random_state,
                os_lim=np.inf, logger=None):
-    if resampling_type == 'under':
-        sel_log('now under sampling ...', None)
-        resampled_index = get_neg_us_index(target, random_state)
-    elif resampling_type == 'over':
-        sel_log('now over sampling ...', None)
-        resampled_index = get_pos_os_index(target, os_lim, random_state)
+    if resampling_type == 'b_under':
+        sel_log('now binary under sampling ...', logger)
+        resampled_index = get_binary_us_index(target, random_state)
+    elif resampling_type == 'b_over':
+        sel_log('now binary over sampling ...', logger)
+        resampled_index = get_binary_os_index(target, os_lim, random_state)
     else:
         sel_log(f'ERROR: wrong resampling type ({resampling_type})', logger)
         sel_log('plz specify "under" or "over".', logger)
