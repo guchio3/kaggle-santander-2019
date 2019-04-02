08cbc60e3fac1da2f4b4f60d4b224314ec6fac93
guchio3 <astt.hwhw@gmail.com>
2019-04-02 10:50:18 +0000

    temp

---
+++ tools/features/f027_f014_3nonuniq.py
@@ -0,0 +1,25 @@
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+
+def f027_f014_3nonuniq(df):
+    res_df = pd.DataFrame()
+    res_df['ID_code'] = df['ID_code']
+    # do not use inplace for re-using
+    df = df.drop(['target'], axis=1)
+    df = df.set_index('ID_code')
+    trn_id = pd.read_pickle('./mnt/inputs/nes_info/trn_ID_code.pkl.gz')
+    tst_id = pd.read_pickle('./mnt/inputs/nes_info/tst_ID_code.pkl.gz')
+    reals = np.load('./mnt/inputs/nes_info/real_samples_indexes.npz.npy')
+    # col rounding
+    for col in tqdm(df.columns):
+        trn_col = df.loc[trn_id][col]
+        real_col = df.loc[tst_id][col].iloc[reals]
+        uniq_cnt_dict = pd.concat(
+            [trn_col, real_col], axis=0).value_counts().to_dict()
+        res_df['non_3uniq_real_' + col] = df[col].apply(
+            lambda x: x if uniq_cnt_dict[x] > 3 else np.nan).values
+    # return as id is set to the index
+    res_df = res_df.set_index('ID_code').add_prefix('f027_')
+    return res_df
