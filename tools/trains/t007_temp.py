ce5b00123d3c54ad74cf1a48bea2177826456d09
guchio3 <astt.hwhw@gmail.com>
2019-04-06 08:07:04 +0000

    temp

---
+++ tools/trains/t007_temp.py
@@ -0,0 +1,138 @@
+import datetime
+import os
+import pickle
+import warnings
+from itertools import tee
+
+import lightgbm
+import numpy as np
+import pandas as pd
+from sklearn.metrics import roc_auc_score
+from sklearn.model_selection import StratifiedKFold
+from tqdm import tqdm
+
+from tools.utils.features import (get_all_features, load_features,
+                                  select_features)
+from tools.utils.logs import dec_timer, log_evaluation, sel_log
+from tools.utils.samplings import value_resampling
+from tools.utils.visualizations import save_importance
+
+warnings.simplefilter(action='ignore', category=FutureWarning)
+
+
+NES_DIR = './mnt/inputs/nes_info/'
+FEATURE_DIR = './mnt/inputs/features/'
+
+
+@dec_timer
+def t007_lgb_train(args, script_name, configs, logger):
+    '''
+    policy
+    ------------
+    * use original functions only if there's no pre-coded functions
+        in useful libraries such as sklearn.
+
+    todos
+    ------------
+    * load features
+    * train the model
+    * save the followings
+        * logs
+        * oofs
+        * importances
+        * trained models
+        * submissions (if test mode)
+
+    '''
+    # -- Load train data
+    sel_log('loading training data ...', None)
+    trn_ids = pd.read_pickle(
+        NES_DIR + 'trn_ID_code.pkl.gz', compression='gzip')
+    tst_ids = pd.read_pickle(
+        NES_DIR + 'tst_ID_code.pkl.gz', compression='gzip')
+    target = pd.read_pickle(
+        NES_DIR + 'target.pkl.gz', compression='gzip')
+    if args.debug:
+        sample_idxes = trn_ids.reset_index(
+            drop=True).sample(
+            random_state=71,
+            frac=0.05).index
+        target = target.iloc[sample_idxes].reset_index(drop=True)
+        trn_ids = trn_ids.iloc[sample_idxes].reset_index(drop=True)
+
+    # load features
+    if configs['train']['all_features']:
+        _features = get_all_features(FEATURE_DIR)
+    else:
+        _features = configs['features']
+    trn_tst_df = load_features(_features, FEATURE_DIR, logger=logger)\
+        .set_index('ID_code')
+
+    # feature selection if needed
+    if configs['train']['feature_selection']:
+        trn_tst_df = select_features(trn_tst_df,
+                                     configs['train']['feature_select_path'],
+                                     configs['train']['metric'],
+                                     configs['train']['feature_topk'])
+    # split train and test
+    sel_log(f'now splitting the df to train and test ones ...', None)
+    features_df = trn_tst_df.loc[trn_ids].reset_index(drop=True)
+    test_features_df = trn_tst_df.loc[tst_ids].reset_index(drop=True)
+
+    # -- Split using group k-fold w/ shuffling
+    # NOTE: this is not stratified, I wanna implement it in the future
+    if configs['train']['fold_type'] == 'skf':
+        skf = StratifiedKFold(n_splits=10, shuffle=True, random_state=4221)
+        folds = skf.split(features_df, target)
+        configs['train']['single_model'] = False
+    else:
+        print(f"ERROR: wrong fold_type, {configs['train']['fold_type']}")
+    folds, pred_folds = tee(folds)
+
+    # -- Make training dataset
+    # print shape
+    sel_log(f'used features are {features_df.columns.tolist()}', logger)
+    sel_log(f'the shape features_df is {features_df.shape}', logger)
+
+    # -- CV
+    # Set params
+    PARAMS = configs['lgbm_params']
+    PARAMS['nthread'] = os.cpu_count()
+
+    sel_log('start training ...', None)
+    with open('./mnt/trained_models/e071_e036_t006_w_pair_augmentation_7_7_2019-04-05-17-35-24_0.92357_models.pkl', 'rb') as fin:
+        cv_model = pickle.load(fin)
+
+    # --- Make submission file
+    if args.test:
+        # -- Prediction
+        sel_log('predicting for test ...', None)
+        preds = []
+        preds_no_rank = []
+        reals = np.load('./mnt/inputs/nes_info/real_samples_indexes.npz.npy')
+        # for booster in tqdm(cv_model.boosters):
+        for booster in tqdm(cv_model):
+            pred = booster.predict(test_features_df.values, num_iteration=None)
+            pred = pd.Series(pred)
+            # rank avg only using real part
+            preds_no_rank.append(pred.copy())
+            pred.iloc[reals] = pred.iloc[reals].rank() / reals.shape
+            preds.append(pred)
+        if len(cv_model) > 1:
+            target_values_no_rank = np.mean(preds_no_rank, axis=0)
+        else:
+            target_values_no_rank = preds[0]
+
+        # -- Make submission file
+        sel_log(f'loading sample submission file ...', None)
+        sub_df_no_rank = pd.read_csv(
+            './mnt/inputs/origin/sample_submission.csv.zip',
+            compression='zip')
+        sub_df_no_rank.target = target_values_no_rank
+
+        # print stats
+        submission_filename_no_rank = f'./mnt/submissions/temp_sub_no_rank.csv'
+        sub_df_no_rank.to_csv(
+            submission_filename_no_rank,
+            index=False)
+    return None, None, None
---
+++ tools/utils/metrics.py
@@ -0,0 +1,38 @@
+import numpy as np
+from tqdm import tqdm
+
+
+def calc_CM(y_true, y_pred, thresh, epsilon=1e-7):
+    # make it 0 or 1 of pos and neg
+    y_pos_true = y_true
+    y_pos_pred = y_pred > thresh
+    y_neg_true = 1 - y_pos_true
+    y_neg_pred = 1 - y_pos_pred
+
+    # calc tp, tn, fp, fn
+    tp = np.sum(y_pos_true * y_pos_pred)
+    tn = np.sum(y_neg_true * y_neg_pred)
+    fp = np.sum(y_neg_true * y_pos_pred)
+    fn = np.sum(y_pos_true * y_neg_pred)
+    return tp, tn, fp, fn
+
+
+def calc_best_CM(y_trues, y_preds, bins=300):
+    best_threshs = []
+    # calc best MCC for each fold
+    for y_true, y_pred in zip(y_trues, y_preds):
+        _best_AUC = 0.
+        best_thresh = 0.
+        for thresh in tqdm(np.linspace(0.001, 0.999, bins)):
+            AUC = np.mean(y_true == y_pred > thresh)
+            if AUC > _best_AUC:
+                _best_AUC = AUC
+                best_thresh = thresh
+        best_threshs.append(best_thresh)
+    # calc best MCC for the whole data
+    whole_y_true = np.concatenate(y_trues, axis=0)
+    _y_preds = [(y_pred > best_thresh).astype(int)
+                for y_pred, best_thresh in zip(y_preds, best_threshs)]
+    whole_y_pred = np.concatenate(_y_preds, axis=0)
+    tp, tn, fp, fn = calc_CM(whole_y_true, whole_y_pred, 0.5)
+    return tp, tn, fp, fn, best_threshs, _best_AUC
