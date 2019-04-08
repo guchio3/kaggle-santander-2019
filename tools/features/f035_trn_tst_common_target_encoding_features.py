ec1eb1f64a47a769a2c4dbb675061a51846e8115
guchio3 <astt.hwhw@gmail.com>
2019-04-08 05:16:44 +0000

    temp

--- tools/features/f035_trn_tst_common_target_encoding_features.py
+++ tools/features/f035_trn_tst_common_target_encoding_features.py
@@ -23,33 +23,44 @@ def f035_trn_tst_common_target_encoding_features(df):
         trn_values = set(trn_col.unique().tolist())
         real_values = set(real_col.unique().tolist())
         valid_values = trn_values & real_values
-        res_df['trn_tst_non_common_' + col] = df[col].apply(
+        res_df['trn_tst_common_target_encoding_features_' + col] = df[col].apply(
             lambda x: x if x in valid_values else np.nan).values
     # make fit_df
     res_df = res_df.set_index('ID_code')
     trn_res_df = res_df.loc[trn_id].copy()
     tst_res_df = res_df.loc[tst_id].copy()
     # fold-wise target encoding
-    for col in tqdm(res_df.columns):
+    for i, col in tqdm(enumerate(res_df.columns)):
         # make fold
         target_col = trn_res_df[col]
-        tst_target_col = tst_res_df[col]
+#        tst_target_col = tst_res_df[col]
         skf = StratifiedKFold(10, random_state=71)
         folds = skf.split(target_col, target)
         for trn_idx, val_idx in folds:
-            _, enc_val = target_encode(
-                target_col.iloc[trn_idx],
-                target_col.iloc[val_idx],
-                target.iloc[trn_idx],
-            )
-            target_col.iloc[val_idx] = enc_val
-        trn_res_df[col] = target_col
-        _, tst_val = target_encode(
-                target_col,
-                tst_target_col,
-                target,
-            )
-        tst_res_df[col] = tst_val
+            #            _, enc_val = target_encode(
+            #                target_col.iloc[trn_idx],
+            #                target_col.iloc[val_idx],
+            #                target.iloc[trn_idx],
+            #                smoothing=0.5,
+            #                noise_level=0.01,
+            #            )
+            enc_dict = pd.concat([target_col.reset_index(drop=True).iloc[trn_idx], target.reset_index(
+                drop=True).iloc[trn_idx]], axis=1).groupby(target_col.name).mean().to_dict()['target']
+#            trn_res_df.iloc[val_idx, i] = enc_val.values
+            trn_res_df.iloc[val_idx, i] = trn_res_df.iloc[val_idx, i].apply(
+                lambda x: enc_dict[x] if x in enc_dict else np.nan).values
+#        _, tst_val = target_encode(
+#            target_col,
+#            tst_target_col,
+#            target,
+#            smoothing=0.5,
+#            noise_level=0.01,
+#        )
+        enc_dict = pd.concat([target_col.reset_index(drop=True), target.reset_index(
+            drop=True)], axis=1).groupby(target_col.name).mean().to_dict()['target']
+        tst_res_df[col] = trn_res_df[col].apply(
+            lambda x: enc_dict[x] if x in enc_dict else np.nan).values
+#        tst_res_df[col] = tst_val.values
     res_df = pd.concat([trn_res_df, tst_res_df], axis=0)
     res_df = res_df.reset_index()
     # return as id is set to the index
